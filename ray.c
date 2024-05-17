1d1dda32421ee5314eadfecadc56477bb67a80bc
Alvin Chan <altc.alchan@gmail.com>
2024-05-16 20:34:56 -0400

    Refactor structures and change use of vectors in algorithms

--- ray.c
+++ ray.c
@@ -3,6 +3,13 @@
 
 #include "raylib.h"
 
+#define PLAYER_FOV 60
+#define PLAYER_SPEED 2
+#define PLAYER_ROT_RATE 3.5
+
+const int SCALE = 2;
+const int PRECISION = 64;
+
 const char GAME_MAP[8][8] = {
     {1,1,1,1,1,1,1,1},
     {1,0,0,0,0,0,0,1},
@@ -14,75 +21,48 @@ const char GAME_MAP[8][8] = {
     {1,1,1,1,1,1,1,1},
 };
 
-const int SCALE = 2;
-
 typedef struct {
     int width;
     int height;
 } Window;
 
 typedef struct {
-    // TODO: reduce to vec2
+    float x;
+    float y;
+} Point;
+
+typedef struct {
     float x;
     float y;
     float angle;
+} Vec2;
+
+typedef struct {
+    // TODO: reduce to vec2
+    Vec2 pos;
     int fov;
     float speed;
     float rotation_rate;
 } Character;
 
-const int PRECISION = 64;
-
 float radians(float degree) {
     return degree * M_PI / 180;
 }
 
-Vec2 add(Vec2 a, Vec2 b) { // TODO: mutable version? scrap?
-    Vec2 temp = {a.x + b.x, a.y + b.y};
-    return temp;
-}
-
-Vec2 sub(Vec2 a, Vec2 b) {
-    Vec2 temp = {a.x - b.x, a.y - b.y};
-    return temp;
-}
-
-float dot(Vec2 a, Vec2 b) {
-    return a.x*b.x + a.y*b.y;;
-}
-
-Vec2 divide(Vec2 v, float m) {
-    Vec2 temp = {v.x/m, v.y/m};
-    return temp;
-}
-
-float mag(Vec2 v) {
-    return sqrt(dot(v, v));
-}
-
-Vec2 normalize(Vec2 v) { // does this mutate my vector?
-    // should this be a ptr return?
-    Vec2 temp = divide(v, mag(v));
-    return temp;
-}
-
-Vec2 get_vec_step(Vec2 v) { // TODO: figure out how to not recalc per step
-    return divide(normalize(v), PRECISION);
-}
-
-float get_distance(Vec2 v, Vec2 ref) { // TODO: convert to ptr passing
-    float distance = mag(sub(v, ref));
-    // TODO: fisheye fix (uhh fuck vectors)
-    return distance;
+Vec2 get_vec_step(Vec2 *ray) {
+    float x_step = cos(radians(ray->angle)) / PRECISION;
+    float y_step = sin(radians(ray->angle)) / PRECISION;
+    Vec2 step = {x_step, y_step, ray->angle};
+    return step;
 }
 
 void raywalk(Vec2 *ray) {
     // save step vec for this ray iteration
-    Vec2 v_step = get_vec_step(*ray);
+    Vec2 step = get_vec_step(ray);
 
     while (GAME_MAP[(int) floor(ray->y)][(int) floor(ray->x)] == 0) {
-	ray->x += v_step.x;
-	ray->y += v_step.y;
+	ray->x += step.x;
+	ray->y += step.y;
     }
 }
 
@@ -92,17 +72,22 @@ void draw_ray(Window *screen, int x, float height) {
     DrawLine(x, (float) screen->height/2 + height, x, screen->height, BLUE);
 }
 
-void raycast(Window *screen, Character *camera) {
-    float view_angle = camera->angle - (float) camera->fov/2;
+float get_distance(Vec2 *ray, Vec2 *ref_pos) {
+    float distance = sqrt((ray->x - ref_pos->x)*(ray->x - ref_pos->x) + (ray->y - ref_pos->y)*(ray->y - ref_pos->y));
+    distance *= cos(radians(ray->angle - ref_pos->angle)); /* fisheye fix */
+    return distance;
+}
+
+void raycast(Window *screen, Character *player) {
+    float view_angle = player->pos.angle - (float) player->fov/2;
 
     for (int i = 0; i < screen->width; i++) {
-	Vec2 ray = {camera->pos.x, camera->pos.y};
+	Vec2 ray = {player->pos.x, player->pos.y, player->pos.angle};
 	raywalk(&ray);
-	float distance = get_distance(ray, camera->pos);
-	distance *= cos(radians(view_angle - camera->angle));
+	float distance = get_distance(&ray, &player->pos);
 	float height = screen->height / (2*distance);
 	draw_ray(screen, i, height);
-	view_angle += (float) camera->fov / screen->width;
+	view_angle += (float) player->fov / screen->width;
     }
 }
 
@@ -112,8 +97,9 @@ int main(void) {
     InitWindow(window.width, window.height, "RayC");
     SetTargetFPS(60);
 
-    Vec2 player_pos = {2, 2};
-    Character player = {player_pos, 90, 60, 2, 3.5};
+    // TODO: make a pointer?
+    Vec2 player_pos = {2, 2, 90};
+    Character player = {player_pos, PLAYER_FOV, PLAYER_SPEED, PLAYER_ROT_RATE};
 
     while (!WindowShouldClose()) {
 	BeginDrawing();
